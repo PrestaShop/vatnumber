01c84f827fd3be6ae1600cab0787d13231839487
Julien Bourdeau <julien@sigerr.org>
2014-04-18 17:05:53 +0200

    // update version for release

--- vatnumber.php
+++ vatnumber.php
@@ -33,7 +33,7 @@ class VatNumber extends TaxManagerModule
 	{
 		$this->name = 'vatnumber';
 		$this->tab = 'billing_invoicing';
-		$this->version = '1.6';
+		$this->version = '1.7';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
